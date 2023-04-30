92458a4a637dd344e405525232fecba1558ccb04
kosoriom <kosoriom@unal.edu.co>
2023-04-29 22:59:38 -0500

    USART
    Se realizó el driver del USART

--- PeripheralDrivers/Src/USARTxDriver.c
+++ PeripheralDrivers/Src/USARTxDriver.c
@@ -8,6 +8,7 @@
 #include <stm32f4xx.h>
 #include "USARTxDriver.h"
 
+
 /**
  * Configurando el puerto Serial...
  * Recordar que siempre se debe comenzar con activar la señal de reloj
@@ -19,13 +20,24 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
     /* 1.1 Configuramos el USART1 */
 	if(ptrUsartHandler->ptrUSARTx == USART1){
 		// Escriba acá su código
+		// Registro del RCC que nos activa la señal del reloj para el USART1
+		RCC->APB2ENR &= ~RCC_APB2ENR_USART1EN;
+		RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 	}
 
     /* 1.2 Configuramos el USART2 */
-    // Escriba acá su código
+	else if(ptrUsartHandler->ptrUSARTx == USART2){
+		// Registro del RCC que nos activa la señal del reloj para el USART2
+		RCC->APB1ENR &= ~RCC_APB1ENR_USART2EN;
+		RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	}
 
     /* 1.3 Configuramos el USART2 */
-    // Escriba acá su código
+	else if(ptrUsartHandler->ptrUSARTx == USART6){
+		// Registro del RCC que nos activa la señal del reloj para el USART6
+		RCC->APB2ENR &= ~RCC_APB2ENR_USART6EN;
+		RCC->APB2ENR |= RCC_APB2ENR_USART6EN;
+	}
 
 	/* 2. Configuramos el tamaño del dato, la paridad y los bit de parada */
 	/* En el CR1 estan parity (PCE y PS) y tamaño del dato (M) */
@@ -47,44 +59,65 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 		if(ptrUsartHandler->USART_Config.USART_parity == USART_PARITY_EVEN){
 			// Es even, entonces cargamos la configuracion adecuada
 			// Escriba acá su código
-
-		}else{
+			ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_PCE;
+			ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_PS;  //Limpiar PS bit para Even parity
+			ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_PEIE; //Configurar la interrupción de error de paridad
+		}else if(ptrUsartHandler->USART_Config.USART_parity == USART_PARITY_ODD){
 			// Si es "else" significa que la paridad seleccionada es ODD, y cargamos esta configuracion
 			// Escriba acá su código
+			 ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_PCE | USART_CR1_PS;
 		}
 	}else{
 		// Si llegamos aca, es porque no deseamos tener el parity-check
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~ USART_CR1_PCE;     // Desactivar el parity-check
 	}
 
 	// 2.3 Configuramos el tamaño del dato
     // Escriba acá su código
+	if(ptrUsartHandler->USART_Config.USART_datasize == USART_DATASIZE_8BIT){
+		// Para 8 bits
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_M;
+	}
+
+	else{
+		// Para 9 bits
+		ptrUsartHandler->ptrUSARTx->CR1 |= ~USART_CR1_M;
+	}
 
 	// 2.4 Configuramos los stop bits (SFR USART_CR2)
 	switch(ptrUsartHandler->USART_Config.USART_stopbits){
 	case USART_STOPBIT_1: {
 		// Debemoscargar el valor 0b00 en los dos bits de STOP
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~USART_CR2_STOP;
 		break;
 	}
 	case USART_STOPBIT_0_5: {
 		// Debemoscargar el valor 0b01 en los dos bits de STOP
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR2 |= USART_CR2_STOP_0;
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~USART_CR2_STOP_1;
+
 		break;
 	}
 	case USART_STOPBIT_2: {
 		// Debemoscargar el valor 0b10 en los dos bits de STOP
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR2 |= USART_CR2_STOP_1;
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~USART_CR2_STOP_0;
 		break;
 	}
 	case USART_STOPBIT_1_5: {
 		// Debemoscargar el valor 0b11 en los dos bits de STOP
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR2 |= USART_CR2_STOP;
 		break;
 	}
 	default: {
 		// En el casopor defecto seleccionamos 1 bit de parada
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~USART_CR2_STOP;
 		break;
 	}
 	}
@@ -103,10 +136,15 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 		// El valor a cargar es 52.0625 -> Mantiza = 52,fraction = 0.0625
 		// Mantiza = 52 = 0x34, fraction = 16 * 0.1875 = 1
 		// Escriba acá su código y los comentarios que faltan
+		ptrUsartHandler->ptrUSARTx->BRR = 0x0341;
 	}
 
 	else if(ptrUsartHandler->USART_Config.USART_baudrate == USART_BAUDRATE_115200){
 		// Escriba acá su código y los comentarios que faltan
+		// valor a cargar es de 8.6875
+		//Mantiza = 8 , fraction = 16* 0.6875 = 11 en hexadecimal es B
+		//Valor a cargar 0x811
+		ptrUsartHandler->ptrUSARTx->BRR = 0x8B;
 	}
 
 	// 2.6 Configuramos el modo: TX only, RX only, RXTX, disable
@@ -115,18 +153,22 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 	{
 		// Activamos la parte del sistema encargada de enviar
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_TE;
+
 		break;
 	}
 	case USART_MODE_RX:
 	{
 		// Activamos la parte del sistema encargada de recibir
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_RE;
 		break;
 	}
 	case USART_MODE_RXTX:
 	{
 		// Activamos ambas partes, tanto transmision como recepcion
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 |= (USART_CR1_TE | USART_CR1_RE);
 		break;
 	}
 	case USART_MODE_DISABLE:
@@ -134,14 +176,19 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 		// Desactivamos ambos canales
 		// Escriba acá su código
 		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_UE;
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_RE;
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_TE;
+
 		break;
 	}
 
 	default:
 	{
 		// Actuando por defecto, desactivamos ambos canales
-		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_RE;
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_RE;
+		ptrUsartHandler->ptrUSARTx->CR1 &= ~USART_CR1_TE;
+
 		break;
 	}
 	}
@@ -149,6 +196,7 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 	// 2.7 Activamos el modulo serial.
 	if(ptrUsartHandler->USART_Config.USART_mode != USART_MODE_DISABLE){
 		// Escriba acá su código
+		ptrUsartHandler->ptrUSARTx->CR1 |= USART_CR1_UE;
 	}
 }
 
@@ -159,6 +207,7 @@ int writeChar(USART_Handler_t *ptrUsartHandler, int dataToSend ){
 	}
 
 	// Escriba acá su código
+	ptrUsartHandler->ptrUSARTx->DR = dataToSend;
 
 	return dataToSend;
 }
---
+++ ProyectoTallerV/App/Src/BasicProject_Main.c
@@ -0,0 +1,25 @@
+ /**
+ ******************************************************************************
+ * @file           : BasicProject_Main.c
+ * @author         : Karol Osorio Monsalve - kosoriom@unal.edu.co
+ * @brief          : Solución básica de un proyecto con librerías externas
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+int main(void){
+	while(1){
+
+	}
+	return 0;
+}
+
+
